958de8e865efc74ba758bf7e58fd5af914e9b30e
scl971009 <dora20823@gmail.com>
2019-12-06 23:04:47 +0800

    modify main code structure (add functions) and add choose character

--- win_the_2020_election.py
+++ win_the_2020_election.py
@@ -2,14 +2,18 @@ import sys
 import pygame
 from pygame.locals import QUIT
 
-def main():
-	pygame.init()
-	window_surface = pygame.display.set_mode((800, 600))
-	pygame.display.set_caption('2020爭霸戰')
+def start(window_surface, background):
+	"""
+	main scene
 
-	background = pygame.image.load("img\\main\\start.png")
-	background = pygame.transform.scale(background, (800, 600))
-	background.convert()
+	arg:
+		background: background image after scaling and convert
+
+	Return value:
+		"start": start button pressed
+		"setting": setting button pressed
+		"instruction": instruction button pressed
+	"""
 	window_surface.blit(background, (0, 0))
 
 	start = pygame.Rect(310, 385, 170, 100)
@@ -25,13 +29,58 @@ def main():
 				sys.exit()
 			elif event.type == pygame.MOUSEBUTTONUP:
 				if event.button == 1:
-					#print(event.pos)
 					if start.collidepoint(event.pos):
-						print("start pressed!")
+						return "start"
 					elif setting.collidepoint(event.pos):
-						print("setting pressed!")
+						print("don't press setting")
 					elif instruction.collidepoint(event.pos):
-						print("instruction pressed!")
+						print("don't press instruction")
+
+
+def choose_player(window_surface, background):
+	"""
+	choose character scene
+
+	arg:
+		choose_char: background image after scaling and convert
+
+	Return Value:
+		korean fish: korean fish is chosen
+	"""
+	window_surface.blit(background, (0, 0))
+	
+	korean_fish = pygame.Rect(320, 180, 180, 330)
+
+	pygame.display.update()
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			elif event.type == pygame.MOUSEBUTTONUP:
+				if event.button == 1:
+					if korean_fish.collidepoint(event.pos):
+						return "korean fish"
+
+def main():
+	pygame.init()
+	window_surface = pygame.display.set_mode((800, 600))
+	pygame.display.set_caption('2020爭霸戰')
+
+	background = pygame.image.load("img\\main\\start.png")
+	background = pygame.transform.scale(background, (800, 600))
+	background.convert()
+	choose_char = pygame.image.load("img\\main\\choose_role.png")
+	choose_char = pygame.transform.scale(choose_char, (800, 600))
+	choose_char.convert()
+
+	button_main = start(window_surface, background)
+
+	if button_main == "start":
+		while True:
+			char = choose_player(window_surface, choose_char)
+			print(char)
 
 if __name__ == '__main__':
 	main()
