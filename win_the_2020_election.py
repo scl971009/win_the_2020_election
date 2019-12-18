292ff1c91dd802ed00e0717517ca2f3963bb3ae3
scl971009 <dora20823@gmail.com>
2019-12-18 17:40:36 +0800

    Actually I don't know what I've changed

--- win_the_2020_election.py
+++ win_the_2020_election.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-from pygame.locals import QUIT
+from pygame.locals import *
 
 from scripts.intelude import Intelude
 from scripts.stage import Stage
@@ -37,18 +37,33 @@ def start(window_surface, background):
 					elif setting.collidepoint(event.pos):
 						print("don't press setting")
 					elif instruction.collidepoint(event.pos):
-						print("don't press instruction")
+						return "instruction"
 
+def rule(window_surface):
+	"""show instruction page"""
+	background = pygame.image.load("img\\else\\rule_HowToPlay.png")
+	background = pygame.transform.scale(background, (800, 600))
+	background.convert()
+
+	window_surface.blit(background, (0, 0))
+
+	pygame.display.update()
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
 
 def choose_player(window_surface, background):
 	"""
 	choose character scene
 
 	arg:
-		choose_char: background image after scaling and convert
+		background: background image after scaling and convert
 
 	Return Value:
-		korean fish: korean fish is chosen
+		0: korean fish is chosen
 	"""
 	window_surface.blit(background, (0, 0))
 	
@@ -79,19 +94,23 @@ def main():
 	choose_char = pygame.transform.scale(choose_char, (800, 600))
 	choose_char.convert()
 
-	button_main = start(window_surface, background)
+	while True:
 
-	if button_main == "start":
-		char = choose_player(window_surface, choose_char)
-		for i in range(5):
-			story = Intelude(window_surface, char, i, 0)
-			story.run()
-			stage = Stage(window_surface, char, i)
-			stage.run()
-			result = stage.get_result()
-			if not result:
-				#fail
-				break
+		button_main = start(window_surface, background)
+
+		if button_main == "start":
+			char = choose_player(window_surface, choose_char)
+			for i in range(5):
+				story = Intelude(window_surface, char, i, 0)
+				story.run()
+				stage = Stage(window_surface, char, i)
+				stage.run()
+				result = stage.get_result()
+				if not result:
+					#fail
+					break
+		elif button_main == "instruction":
+			rule(window_surface)
 
 if __name__ == '__main__':
 	main()
