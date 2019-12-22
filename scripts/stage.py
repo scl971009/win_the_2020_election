1839a37d5aefbe0dbf4975d1efebe7fbe764660b
scl971009 <dora20823@gmail.com>
2019-12-22 21:35:16 +0800

    stage.action()

--- scripts/stage.py
+++ scripts/stage.py
@@ -10,31 +10,8 @@ def set_life(num):
 	life = pygame.transform.scale(life, (60, 70))
 	return life
 
-def stage00(window_surface, background, material_amount, player):
-	"""stage 1 for korea fish"""
-	life = set_life(player.get_life())
-	player.stage_start()
-
-	floor = pygame.Rect(0, 555, 800, 45)
-	block1 = pygame.Rect(95, 140, 130, 20)
-	block2 = pygame.Rect(570, 140, 135, 20)
-	block3 = pygame.Rect(195, 270, 390, 20)
-	block4 = pygame.Rect(150, 405, 500, 25)
-	floor_list = []
-	floor_list.append(floor)
-	floor_list.append(block1)
-	floor_list.append(block2)
-	floor_list.append(block3)
-	floor_list.append(block4)
-
-	enemy0 = Enemy(0, 0, 0)
-	enemy1 = Enemy(0, 0, 1)
-	enemy_group = pygame.sprite.Group()
-	enemy_group.add(enemy0)
-	enemy_group.add(enemy1)
-	material_num = 0
-	material = Material(0, 0, material_num)
-
+def action(character, level, window_surface, player, background, material_num, material_amount, floor_list, enemy_group):
+	material = Material(character, level, material_num)
 	while True:
 		life = set_life(player.get_life())
 		window_surface.blit(background, (0, 0))
@@ -64,22 +41,47 @@ def stage00(window_surface, background, material_amount, player):
 						material_num = material_num + 1
 						if material_num == material_amount:
 							return True
-						material = Material(0, 0, material_num)
+						material = Material(character, level, material_num)
 					
 		pressed_keys = pygame.key.get_pressed()
 		player.move_left_right(pressed_keys)
 		player.update(floor_list, enemy_group)
-		enemy0.update(floor_list, player.get_rect())
-		enemy1.update(floor_list, player.get_rect())
+		for enemy in enemy_group:
+			enemy.update(floor_list, player.get_rect())
 
 		if pygame.sprite.collide_rect(player, material):
 			material_num = material_num + 1
 			if material_num == material_amount:
 				return True
-			material = Material(0, 0, material_num)
+			material = Material(character, level, material_num)
 
 		if player.get_life() == 0:
 			return False
+
+def stage00(window_surface, background, material_amount, player):
+	"""stage 1 for korea fish"""
+	life = set_life(player.get_life())
+	player.stage_start()
+
+	floor = pygame.Rect(0, 555, 800, 45)
+	block1 = pygame.Rect(95, 140, 130, 20)
+	block2 = pygame.Rect(570, 140, 135, 20)
+	block3 = pygame.Rect(195, 270, 390, 20)
+	block4 = pygame.Rect(150, 405, 500, 25)
+	floor_list = []
+	floor_list.append(floor)
+	floor_list.append(block1)
+	floor_list.append(block2)
+	floor_list.append(block3)
+	floor_list.append(block4)
+
+	enemy0 = Enemy(0, 0, 0)
+	enemy1 = Enemy(0, 0, 1)
+	enemy_group = pygame.sprite.Group()
+	enemy_group.add(enemy0)
+	enemy_group.add(enemy1)
+
+	return action(0, 0, window_surface, player, background, 0, material_amount, floor_list, enemy_group)
 
 def stage01(window_surface, background, material_amount, player):
 	"""stage 2 for korea fish"""
@@ -106,54 +108,7 @@ def stage01(window_surface, background, material_amount, player):
 	enemy_group.add(enemy1)
 	enemy_group.add(enemy2)
 	
-	material_num = 0
-	material = Material(0, 1, material_num)
-	
-	while True:
-		life = set_life(player.get_life())
-		window_surface.blit(background, (0, 0))
-		window_surface.blit(life, (450, 545))
-		window_surface.blit(player.get_surf(), player.get_rect())
-		window_surface.blit(material.get_surf(), material.get_rect())
-
-		pygame.display.update()
-
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				pygame.quit()
-				sys.exit()
-			elif event.type == KEYDOWN:
-				if event.key == K_SPACE:
-					print(player.get_player_rect().x, player.get_player_rect().y)
-				elif event.key == K_UP:
-					player.jump()
-		
-			elif event.type == pygame.MOUSEBUTTONUP:
-				if event.button == 1:
-					print(event.pos)
-					
-					if player.get_rect().collidepoint(event.pos):
-						player.life = player.life - 1
-					if material.get_rect().collidepoint(event.pos):
-						material_num = material_num + 1
-						if material_num == material_amount:
-							return True
-						material = Material(0, 1, material_num)
-					
-		pressed_keys = pygame.key.get_pressed()
-		player.move_left_right(pressed_keys)
-		player.update(floor_list, enemy_group)
-		enemy0.update(floor_list, player.get_rect())
-		enemy1.update(floor_list, player.get_rect())
-
-		if pygame.sprite.collide_rect(player, material):
-			material_num = material_num + 1
-			if material_num == material_amount:
-				return True
-			material = Material(0, 1, material_num)
-
-		if player.get_life() == 0:
-			return False
+	return action(0, 1, window_surface, player, background, 0, material_amount, floor_list, enemy_group)
 		
 
 def start(window_surface, character, level, player):
